6f5a3aa7e7b99a002083306eecfa1ea1cbbf4d59
daveit <daveporter@me.com>
2013-12-16 13:44:27 +0800

    assets compiled for Heroku Dec 16

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ Shipping::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
+  # root 'welcome#index'
+  root to: "admin/dashboard#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
